2923d6e12a78cf38a12d9b8acb11bc6f1b5a61a4
Kent Zacatelco <kentzacattelco05@gmail.com>
2015-01-07 11:25:32 -0800

    stuff

---
+++ public_html/script.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$("documents") .ready(function(){
+    $(".a").css("background-color", "yellow");
+    $('p:last').css({"background-color": "red", "color": "blue"});
+    $('p:first').css({"background-color": "purple", "color": "pink"});
+});
