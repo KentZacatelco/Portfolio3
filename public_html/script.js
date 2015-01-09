7f06999578b0e96cf0fb5e637b7f4f9ae025cda5
Kent Zacatelco <kentzacattelco05@gmail.com>
2015-01-09 11:30:02 -0800

    .

--- public_html/script.js
+++ public_html/script.js
@@ -8,10 +8,23 @@ $("documents") .ready(function(){
     $(".a").css({"background-color": "navy"});
     $('p:first').css({"background-color": "blue", "color": "aqua", "text-decoration": "underline"});
     $('p:last').css({"background-color": "green", "color": "lime", "font-style": "italic", "border": "20px solid limegreen"});
-    $('p:last').bind('click', alertButtonClick);
+    $("h3").bind('click', alertButtonClick);
+    $('#a').bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
     
 });
 
 function alertButtonClick() {
     alert("Why didn't you listen? You are now cursed with bad luck, just kidding");
 } 
+
+function mouseOverMe(){
+    $('#a').html("Don't Do It");
+}
+
+function mouseOutMe(){
+    $('#a').html("Don't Click Me");
+}
+
+function mouseClick(){
+    $('p:last').html("hello");
+}
