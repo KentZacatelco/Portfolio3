ecd0da1fc409fcabc16e680594e0f04e2c6f9d08
Kent Zacatelco <kentzacattelco05@gmail.com>
2015-01-22 11:29:04 -0800

    .

--- public_html/script.js
+++ public_html/script.js
@@ -4,12 +4,14 @@
  * and open the template in the editor.
  */
 
-$("documents") .ready(function(){
-    $('#c > body').hide();
-    $('#c').click(function() {
+$("document") .ready(function(){
+    //for the none working acordian
+    $('#c > p').hide();
+    $('#c h5').click(function() {
         $(this).siblings('p:visible').slideUp('2000');
         $(this).next().animate({'height':'toggle'}, '2000', 'easeInOutExpo');
     });
+    //the css designs
     $(".a").css({"background-color": "navy"});
     $(".b").css({"background-color": "purple", "text-color": "white"});
     $('p:first').css({"background-color": "blue", "color": "aqua", "text-decoration": "underline"});
@@ -20,32 +22,20 @@ $("documents") .ready(function(){
     $('#b').css({"background-color": "green", "color": "lime"});
     $('button1').bind('click', alertButtonClick);
     $("div[class]").css("border", "3px solid black");
-///    $("#").accordian({ header: "h4" });
+//  this accordian thing disrupts the pictures
+//    $("#d").accordian({ header: "h5" });
     setInterval("slidePix()", 2000);
 
     
     $('#button1').bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
-    $('#1').bind('click', hideTheImage);
-    $('#2').bind('click', showTheImage);
     $('#3').bind('click', toggleTheText);
-    $('#4').bind('click', fadeTheImage);
-    $('#5').bind('click', fadeALittle);
-    $('#6').bind('click', fullOpacityImage);
-    $('#7').bind('click', slideTheImage);
-    $('#8').bind('click', unslideTheImage);
-    $('#9').bind('click', customAnimate);
-    $('#10').bind('click', shakeLogo);
-    $('#11').bind('click', bounceLogo);
-    $('#12').bind('click', highlightLogo);
-    $('#13').bind('click', pulsateLogo);
-    $('#14').bind('click', sizeLogo);
     $('#Add1').bind('click', addAPara);
     $('#remove1').bind('click', removeAPara);
     
 });
 
 function alertButtonClick() {
-    alert("Why didn't you listen? You are now cursed with bad luck, just kidding.");
+    alert("Pointless");
 } 
 
 function mouseOverMe(){
@@ -60,58 +50,12 @@ function mouseClick(){
     $('p:last').html("hello");
 }
 
-function hideTheImage() {
-    $('#').hide('puff', {}, 2500);
-}
-
-function showTheImage() {
-    $('#').show('fold', {}, 2500);
-}
-
 function toggleTheText() {
     $('h3').toggle(2500);
 }
 
-function fadeTheImage() {
-    $('#').fadeOut(2500);
-}
-function fadeALittle() {
-    $('#').fadeTo(2500, .30);
-}
-
-function fullOpacityImage(){
-    $('#').fadeTo(2500, 1);
-}
-
-function slideTheImage() {
-    $('#').slideUp(2500);
-}
-
-function unslideTheImage() {
-    $('#').slideDown(2500);
-}
-function customAnimate() {
-    $('#').animate({opacity: 0.25, height: '150px'}, 2000, 'easeOutElastic');
-}
-
-function shakeLogo() {
-    $('#').effect('shake', {times: 5, direction: 'down', distance: 10}, 200);
-}
-
-function bounceLogo() {
-    $('#').effect('bounce', {times: 5, direction: 'up', distance: 10}, 200);
-}
-
 function highlightLogo() {
     $('h3').effect('highlight', {color: 'lime'}, 500);
-}
-
-function pulsateLogo() {
-    $('#').effect('pulsate', {times: 500}, 200);
-}
-
-function sizeLogo() {
-    $('#').effect('size', {to: {height: 200, width: 200}});
 }
 
 function addAPara() {
